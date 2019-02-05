bc08eb74f7e61060ea8b2312e874d15deb604636
Lee J Martin-McDonald <lee.j.martin.mcdonald@gmail.com>
2019-02-05 19:53:13 +0100

    Update database to include expiration time;

--- Data/Migrations/20190205184211_AddExpiringTimeToBatchItems.Designer.cs
+++ Data/Migrations/20190205184211_AddExpiringTimeToBatchItems.Designer.cs
@@ -4,14 +4,16 @@ using Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Data.Migrations
 {
     [DbContext(typeof(ProjectEntities))]
-    partial class ProjectEntitiesModelSnapshot : ModelSnapshot
+    [Migration("20190205184211_AddExpiringTimeToBatchItems")]
+    partial class AddExpiringTimeToBatchItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
