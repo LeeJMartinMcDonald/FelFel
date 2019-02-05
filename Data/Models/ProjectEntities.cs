89c74c9ee017e66054ae11166ba22ee6e5868277
Lee J Martin-McDonald <lee.j.martin.mcdonald@gmail.com>
2019-02-05 10:34:13 +0100

    Set up database and database migrations;

---
+++ Data/Models/ProjectEntities.cs
@@ -0,0 +1,33 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Models
+{
+    public partial class ProjectEntities : DbContext
+    {
+        public ProjectEntities(string connectionString)
+            : base()
+        {
+        }
+
+        public ProjectEntities()
+            : base()
+        {
+        }
+
+        public static string ConnectionString { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
+
--- FelFel/Startup.cs
+++ FelFel/Startup.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Data.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,12 +23,33 @@ namespace FelFel
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("ProjectEntities");
+            services.AddDbContext<ProjectEntities>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+            ProjectEntities.ConnectionString = connectionString;
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            using (var scope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                try
+                {
+                    var context = scope.ServiceProvider.GetService<ProjectEntities>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    // rethrow exception after logging 
+                    throw;
+                }
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
