48e880db92d6d074d2a527fe8aaa411530cea1a7
Lee J Martin-McDonald <lee.j.martin.mcdonald@gmail.com>
2019-02-05 20:01:09 +0100

    Add batch service;

---
+++ Application/Services/BatchService.cs
@@ -0,0 +1,29 @@
+﻿using Application.Models;
+using Data.Queries;
+using Entities;
+
+namespace Application.Services
+{
+    public class BatchService : IBatchService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BatchService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void Add(Models.Batch model)
+        {
+            var batch = new Entities.Batch
+            {
+                ExpirationDate = model.ExpirationDate,
+                ExpiringTime = model.ExpiringTime,
+                CheckedInDate = model.CheckedInDate,
+                
+            };
+
+            _unitOfWork.BatchRepository.Add(batch);
+        }
+    }
+}
---
+++ Application/Services/IBatchService.cs
@@ -0,0 +1,9 @@
+﻿using Application.Models;
+
+namespace Application.Services
+{
+    public interface IBatchService
+    {
+        void Add(Batch model);
+    }
+}
