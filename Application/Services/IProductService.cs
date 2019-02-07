5af719447dfed9e266ad97e69e67a5fcbefd4426
Lee J Martin-McDonald <lee.j.martin.mcdonald@gmail.com>
2019-02-07 19:58:00 +0100

    Create product service with method to get all products;

---
+++ Application/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Models;
+
+namespace Application.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetProducts();
+    }
+}
---
+++ Application/Services/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Models;
+using Data.Queries;
+
+namespace Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            var products = await _unitOfWork.ProductRepository.Get();
+            var result = products.Select(x => new Product {
+                Id = x.Id,
+                Name = x.Name
+            });
+
+            return result;
+        }
+    }
+}
