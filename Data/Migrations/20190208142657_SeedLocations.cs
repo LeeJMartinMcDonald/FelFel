d59dafab0827eb801460f4d98b9acd0ed74c2533
Lee J Martin-McDonald <lee.j.martin.mcdonald@gmail.com>
2019-02-08 15:30:04 +0100

    Seed locations;

---
+++ Data/Migrations/20190208142657_SeedLocations.cs
@@ -0,0 +1,40 @@
+﻿using Data.Models;
+using Entities;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class SeedLocations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            using (var context = new ProjectEntities())
+            {
+                var location1 = new Location
+                {
+                    Name = "Fifa"
+                };
+                context.Locations.Add(location1);
+
+                var location2 = new Location
+                {
+                    Name = "Hugo Boss"
+                };
+                context.Locations.Add(location2);
+
+                var location3 = new Location
+                {
+                    Name = "Odlo"
+                };
+                context.Locations.Add(location3);
+
+                context.SaveChanges();
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
