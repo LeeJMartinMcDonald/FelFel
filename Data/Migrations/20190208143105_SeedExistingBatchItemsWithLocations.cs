b53e92c2acb97e4c35bf1731fa8ff97f66d15680
Lee J Martin-McDonald <lee.j.martin.mcdonald@gmail.com>
2019-02-08 15:34:24 +0100

    Seed existing batch items with locations;

---
+++ Data/Migrations/20190208143105_SeedExistingBatchItemsWithLocations.cs
@@ -0,0 +1,29 @@
+﻿using Data.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Data.Migrations
+{
+    public partial class SeedExistingBatchItemsWithLocations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            using (var context = new ProjectEntities())
+            {
+                var firstLocation = context.Locations.FirstOrDefault();
+
+                context.BatchItems.ToList().ForEach(item =>
+                {
+                    item.Location = firstLocation;
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
