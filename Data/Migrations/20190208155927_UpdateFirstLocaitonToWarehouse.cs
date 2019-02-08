09c0f93d07ac041c0a8a260fd17bd4192074a43f
Lee J Martin-McDonald <lee.j.martin.mcdonald@gmail.com>
2019-02-08 17:06:02 +0100

    Update first location from Fifa to Warehouse;

---
+++ Data/Migrations/20190208155927_UpdateFirstLocaitonToWarehouse.cs
@@ -0,0 +1,25 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Linq;
+
+namespace Data.Migrations
+{
+    public partial class UpdateFirstLocaitonToWarehouse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            using (var context = new ProjectEntities())
+            {
+                var location = context.Locations.FirstOrDefault();
+                location.Name = "Warehouse";
+
+                context.SaveChanges();
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
