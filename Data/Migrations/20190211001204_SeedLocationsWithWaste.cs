3fbb30252a27378c15e652988128b5c0d8825830
Lee J Martin-McDonald <lee.j.martin.mcdonald@gmail.com>
2019-02-11 01:15:40 +0100

    Seed locations with waste;

---
+++ Data/Migrations/20190211001204_SeedLocationsWithWaste.cs
@@ -0,0 +1,35 @@
+﻿using Data.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Linq;
+
+namespace Data.Migrations
+{
+    public partial class SeedLocationsWithWaste : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            using (var context = new ProjectEntities())
+            {
+                var location = context.Locations.FirstOrDefault(x => x.Id == 2);
+                location.Name = "Waste";
+
+                context.Locations.Add(new Entities.Location
+                {
+                    Name = "Fifa"
+                });
+
+                context.Locations.Add(new Entities.Location
+                {
+                    Name = "Hugo Boss"
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
